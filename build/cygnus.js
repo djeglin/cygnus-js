bf9ced9e058c320ddfa55c9df0048d61d414b195
Jeff Escalante <hello@jenius.me>
2016-07-14 11:49:28 -0400

    use correct presets with babel build

--- build/cygnus.js
+++ build/cygnus.js
@@ -1,6 +1,8 @@
+'use strict';
+
 /* global Blob, Worker, XMLHttpRequest */
 
-const _arguments = arguments;
+var _arguments = arguments;
 
 module.exports = {
   supportsHistory: !!window.history,
@@ -8,7 +10,9 @@ module.exports = {
   supportsPromises: !!Promise,
   ready: false,
   pages: {},
-  init: function () {
+  init: function init() {
+    var _this = this;
+
     // Exit if history api, workers and promises aren't all supported
     if (!this.supportsHistory || !this.supportsWorkers || !this.supportsPromises) {
       console.info('[Cygnus]: cygnus is not supported in this browser.');
@@ -23,83 +27,119 @@ module.exports = {
 
     // Start up the worker if it hasn't already been started
     if (typeof this.cygnusWorker === 'undefined') {
-      const workerSrc = document.querySelector('[data-cygnus-worker]').getAttribute('data-src');
-      this.ajaxPromise(workerSrc).then(response => {
-        const blob = new Blob([response]);
-        this.cygnusWorker = new Worker(window.URL.createObjectURL(blob));
-        this.completeInit();
-      }, error => {
+      var workerSrc = document.querySelector('[data-cygnus-worker]').getAttribute('data-src');
+      this.ajaxPromise(workerSrc).then(function (response) {
+        var blob = new Blob([response]);
+        _this.cygnusWorker = new Worker(window.URL.createObjectURL(blob));
+        _this.completeInit();
+      }, function (error) {
         console.error('[Cygnus]: Worker initialisation failed!', error);
       });
     } else {
       this.completeInit();
     }
   },
-  completeInit: function () {
+  completeInit: function completeInit() {
+    var _this2 = this;
+
     // Respond to the worker
-    this.cygnusWorker.onmessage = e => {
-      this.receivePageData(JSON.parse(e.data));
+    this.cygnusWorker.onmessage = function (e) {
+      _this2.receivePageData(JSON.parse(e.data));
     };
 
     // Add current page without re-fectching it
     if (!this.pages[window.location.href]) this.getCurrentPage();
 
     // Get list of links and send them off to the worker
-    const links = this.getLinks();
-    for (const k of links) {
-      links.map(() => this.dispatchLink(k, links[k]));
+    var links = this.getLinks();
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
+
+    try {
+      var _loop = function _loop() {
+        var k = _step.value;
+
+        links.map(function () {
+          return _this2.dispatchLink(k, links[k]);
+        });
+      };
+
+      for (var _iterator = links[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+        _loop();
+      }
+
+      // Handle clicks on links
+    } catch (err) {
+      _didIteratorError = true;
+      _iteratorError = err;
+    } finally {
+      try {
+        if (!_iteratorNormalCompletion && _iterator.return) {
+          _iterator.return();
+        }
+      } finally {
+        if (_didIteratorError) {
+          throw _iteratorError;
+        }
+      }
     }
 
-    // Handle clicks on links
     this.catchLinks(links);
   },
-  getCurrentPage: function () {
+  getCurrentPage: function getCurrentPage() {
     console.info("[Cygnus]: Current page isn't in store. Adding from html already loaded in browser.");
     // Add the current page's html to the store
     this.pages[window.location.href] = this.parseHTML(document.documentElement.outerHTML);
-    const messageData = { task: 'add', link: window.location.href };
+    var messageData = { task: 'add', link: window.location.href };
     // Notify the worker that this page doesn't need to be fetched
     this.cygnusWorker.postMessage(JSON.stringify(messageData));
   },
-  getLinks: function () {
-    let documentLinks = document.querySelectorAll('a[href]');
+  getLinks: function getLinks() {
+    var documentLinks = document.querySelectorAll('a[href]');
     documentLinks = Array.prototype.slice.call(documentLinks, 0);
     return documentLinks.filter(this.filterLinks);
   },
-  filterLinks: function (link) {
+  filterLinks: function filterLinks(link) {
     return link.hostname === window.location.hostname;
   },
-  dispatchLink: function (key, link) {
+  dispatchLink: function dispatchLink(key, link) {
     // We don't dispatch the link to the worker if it has already been fetched
     if (!this.pages[link]) {
-      const messageData = { task: 'fetch', link: link.href };
+      var messageData = { task: 'fetch', link: link.href };
       this.cygnusWorker.postMessage(JSON.stringify(messageData));
     }
   },
-  catchLinks: function (links) {
-    links.forEach((link, i) => {
+  catchLinks: function catchLinks(links) {
+    var _this3 = this;
+
+    links.forEach(function (link, i) {
       // We clone these links in case they already have eventlisteners applied.
       // This removes them
-      const clone = link.cloneNode(true);
+      var clone = link.cloneNode(true);
       link.parentNode.replaceChild(clone, link);
-      clone.addEventListener('click', e => {
+      clone.addEventListener('click', function (e) {
         e.preventDefault();
-        if (this.href !== window.location.href) {
-          this.startLoadPage(this.href, true);
+        if (_this3.href !== window.location.href) {
+          _this3.startLoadPage(_this3.href, true);
         }
       });
     });
   },
-  handlePopState: function (event) {
+  handlePopState: function handlePopState(event) {
     if (this.ready) {
       this.startLoadPage(document.location);
       return true;
     }
   },
-  startLoadPage: function (href, click = false) {
+  startLoadPage: function startLoadPage(href) {
+    var _this4 = this;
+
+    var click = arguments.length <= 1 || arguments[1] === undefined ? false : arguments[1];
+
     // Get the page from the store. We use "cygnus" rather than "this" here as
     // this method can be called from outside the local scope
-    const page = this.pages[href];
+    var page = this.pages[href];
 
     // If the requested page isn't in the store for some reason, navigate as
     // normal
@@ -109,18 +149,20 @@ module.exports = {
     }
 
     // Outro animation...
-    const outro = page.querySelector('body').getAttribute('data-outro');
+    var outro = page.querySelector('body').getAttribute('data-outro');
     if (outro && !!this.isFunction(outro, window)) {
-      this.getFunction(outro, window).then(response => {
-        this.completeLoadPage(href, click, page);
-      }, () => {
+      this.getFunction(outro, window).then(function (response) {
+        _this4.completeLoadPage(href, click, page);
+      }, function () {
         console.error('[Cygnus]: Outro animation promise errorred. Broken :(');
       });
     } else {
       this.completeLoadPage(href, click, page);
     }
   },
-  completeLoadPage: function (href, click, page) {
+  completeLoadPage: function completeLoadPage(href, click, page) {
+    var _this5 = this;
+
     // If we get this far, the page is in the store and we should update the
     // history object
     if (click) window.history.pushState({ url: href }, '', href);
@@ -129,8 +171,8 @@ module.exports = {
     document.title = page.querySelector('title').innerText;
 
     // Set animation attributes on body tag
-    let outro = page.querySelector('body').getAttribute('data-outro');
-    let intro = page.querySelector('body').getAttribute('data-intro');
+    var outro = page.querySelector('body').getAttribute('data-outro');
+    var intro = page.querySelector('body').getAttribute('data-intro');
 
     if (outro) {
       document.body.setAttribute('data-outro', outro);
@@ -146,12 +188,12 @@ module.exports = {
 
     // Remove any per-page css file if needed, and add the new one from the page
     // to be loaded if present
-    const documentStylesheet = document.querySelector("link[data-rel='page-css']");
+    var documentStylesheet = document.querySelector("link[data-rel='page-css']");
     if (documentStylesheet) {
       documentStylesheet.parentNode.removeChild(documentStylesheet);
     }
 
-    const pageStylesheet = page.querySelector("link[data-rel='page-css']");
+    var pageStylesheet = page.querySelector("link[data-rel='page-css']");
     if (pageStylesheet) {
       document.querySelector('head').appendChild(pageStylesheet.cloneNode(true));
     }
@@ -159,26 +201,26 @@ module.exports = {
     // Replace only the content within our page wrapper, as the stuff outside
     // that will remain unchanged
     // TODO: Think about whether we need to change body classes etc
-    const wrapper = document.querySelector('.wrap');
-    const pageContent = page.querySelector('.wrap').cloneNode(true).innerHTML;
+    var wrapper = document.querySelector('.wrap');
+    var pageContent = page.querySelector('.wrap').cloneNode(true).innerHTML;
     wrapper.innerHTML = pageContent;
 
     // Intro animation...
     intro = page.querySelector('body').getAttribute('data-intro');
     if (intro && !!this.isFunction(intro, window)) {
-      this.getFunction(intro, window).then(response => {
+      this.getFunction(intro, window).then(function (response) {
         // Re-run the init method. This time it won't start the worker (it is
         // already running). Basically it will just check for new links and
         // dispatch them to the worker if needed
-        this.init();
-      }, () => {
+        _this5.init();
+      }, function () {
         console.error('[Cygnus]: Intro animation promise errorred. Broken :(');
       });
     } else {
       this.init();
     }
   },
-  receivePageData: function (data) {
+  receivePageData: function receivePageData(data) {
     // Add received page to the store
     this.pages[data.link] = this.parseHTML(data.html);
   },
@@ -191,12 +233,12 @@ module.exports = {
   // usage the script will need to be self contained, so I moved them here.
   //
 
-  ajaxPromise: function (url) {
-    return new Promise((resolve, reject) => {
-      const req = new XMLHttpRequest();
+  ajaxPromise: function ajaxPromise(url) {
+    return new Promise(function (resolve, reject) {
+      var req = new XMLHttpRequest();
       req.open('GET', url);
 
-      req.onload = () => {
+      req.onload = function () {
         if (req.status === 200) {
           resolve(req.response);
         } else {
@@ -204,33 +246,77 @@ module.exports = {
         }
       };
 
-      req.onerror = () => {
+      req.onerror = function () {
         reject(new Error('Network Error'));
       };
 
       req.send();
     });
   },
-  parseHTML: function (string) {
-    const tmp = document.implementation.createHTMLDocument('temp');
+  parseHTML: function parseHTML(string) {
+    var tmp = document.implementation.createHTMLDocument('temp');
     tmp.documentElement.innerHTML = string;
     return tmp.documentElement;
   },
-  isFunction: function (functionName, context) {
-    const namespaces = functionName.split('.');
-    const func = namespaces.pop();
-    for (let k of namespaces) {
-      context = context[namespaces[k]];
+  isFunction: function isFunction(functionName, context) {
+    var namespaces = functionName.split('.');
+    var func = namespaces.pop();
+    var _iteratorNormalCompletion2 = true;
+    var _didIteratorError2 = false;
+    var _iteratorError2 = undefined;
+
+    try {
+      for (var _iterator2 = namespaces[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+        var _k = _step2.value;
+
+        context = context[namespaces[_k]];
+      }
+    } catch (err) {
+      _didIteratorError2 = true;
+      _iteratorError2 = err;
+    } finally {
+      try {
+        if (!_iteratorNormalCompletion2 && _iterator2.return) {
+          _iterator2.return();
+        }
+      } finally {
+        if (_didIteratorError2) {
+          throw _iteratorError2;
+        }
+      }
     }
+
     return typeof context[func] === 'function';
   },
-  getFunction: function (functionName, context) {
-    const args = [].slice.call(_arguments).splice(2);
-    const namespaces = functionName.split('.');
-    const func = namespaces.pop();
-    for (let k of namespaces) {
-      context = context[namespaces[k]];
+  getFunction: function getFunction(functionName, context) {
+    var args = [].slice.call(_arguments).splice(2);
+    var namespaces = functionName.split('.');
+    var func = namespaces.pop();
+    var _iteratorNormalCompletion3 = true;
+    var _didIteratorError3 = false;
+    var _iteratorError3 = undefined;
+
+    try {
+      for (var _iterator3 = namespaces[Symbol.iterator](), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
+        var _k2 = _step3.value;
+
+        context = context[namespaces[_k2]];
+      }
+    } catch (err) {
+      _didIteratorError3 = true;
+      _iteratorError3 = err;
+    } finally {
+      try {
+        if (!_iteratorNormalCompletion3 && _iterator3.return) {
+          _iterator3.return();
+        }
+      } finally {
+        if (_didIteratorError3) {
+          throw _iteratorError3;
+        }
+      }
     }
+
     if (context[func]) {
       return context[func].apply(context, args);
     } else {
