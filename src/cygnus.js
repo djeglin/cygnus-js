a702c553a6af7b60d9ac25fb093f648ff722649f
David Eglin <djeglin@gmail.com>
2016-07-19 11:12:18 +0100

    Inlined worker code to remove dependency on external file

--- src/cygnus.js
+++ src/cygnus.js
@@ -30,14 +30,16 @@ var cygnus = module.exports = {
 
     // Start up the worker if it hasn't already been started
     if (typeof cygnus.cygnusWorker === 'undefined') {
-      const workerSrc = document.querySelector('[data-cygnus-worker]').getAttribute('data-src');
-      cygnus.ajaxPromise(workerSrc).then(response => {
-        const blob = new Blob([response]);
-        cygnus.cygnusWorker = new Worker(window.URL.createObjectURL(blob));
-        cygnus.completeInit();
-      }, error => {
-        console.error('[Cygnus]: Worker initialisation failed!', error);
-      });
+      // const workerSrc = document.querySelector('[data-cygnus-worker]').getAttribute('data-src');
+      // cygnus.ajaxPromise(workerSrc).then(response => {
+      //   const blob = new Blob([response]);
+      //   cygnus.cygnusWorker = new Worker(window.URL.createObjectURL(blob));
+      //   cygnus.completeInit();
+      // }, error => {
+      //   console.error('[Cygnus]: Worker initialisation failed!', error);
+      // });
+      cygnus.cygnusWorker = new Worker(cygnus.workerBlob);
+      cygnus.completeInit();
     } else {
       cygnus.completeInit();
     }
@@ -247,5 +249,53 @@ var cygnus = module.exports = {
     } else {
       return false;
     }
-  }
-};
+  },
+
+  workerBlob: URL.createObjectURL( new Blob([ 
+    `const fetchedPages = [];
+    self.onmessage = msg => {
+      const data = JSON.parse(msg.data);
+
+      if (data.task === 'fetch') {
+        console.info("[Cygnus worker]: Fetching " + data.link);
+        if (fetchedPages.indexOf(data.link) < 0) {
+          getPage(data.link).then(response => {
+            fetchedPages.push(data.link);
+            sendToBrowser({ link: data.link, html: response });
+          }, error => {
+            console.error('[Cygnus worker]: Failed!', error);
+          });
+        }
+      }
+      if (data.task === 'add') {
+        console.info("[Cygnus worker]: Adding " + data.link + " to list without fetching.");
+        if (fetchedPages.indexOf(data.link) < 0) {
+          fetchedPages.push(data.link);
+        }
+      }
+    }
+    function getPage(url) {
+      return new Promise((resolve, reject) => {
+        const req = new XMLHttpRequest();
+        req.open('GET', url);
+
+        req.onload = () => {
+          if (req.status === 200) {
+            resolve(req.response);
+          } else {
+            reject(new Error(req.statusText));
+          }
+        };
+
+        req.onerror = () => {
+          reject(new Error('Network Error'));
+        };
+
+        req.send();
+      });
+    }
+    function sendToBrowser(data) {
+      self.postMessage(JSON.stringify(data));
+    }`
+  ], {type: 'application/javascript'} ))
+};    
